fa8e0b9f729ea876e0f269b1e8f5ce6da4e475c8
Babtsov <ben.babtsov@ufl.edu>
2016-09-17 21:37:19 +0000

    Enable kernel recognize all possible commands for plog

--- servers/pm/plog.c
+++ servers/pm/plog.c
@@ -18,21 +18,37 @@ struct plog_table {
 int do_plog(void) {
     int command = m_in.plog_cmd;
     switch(command) {
+
         case STOP_PLOG:
             printf("plog was called with STOP_PLOG\n");
             break;
 
+        case START_PLOG:
+            printf("plog was called with START_PLOG\n");
+            break;
+
+        case RESET_PLOG:
+            printf("plog was called with RESET_PLOG\n");
+            break;
+
+        case GET_PLOG_SIZE:
+            printf("plog was called with GET_PLOG_SIZE\n");
+            mp->mp_reply.plog_int = PLOG_TABLE_SIZE;
+            break;
+
         case GET_PLOG_BYINDX:
-            printf("plog was called with GET_PLOG_BY_INDX\n");
+            printf("plog was called with GET_PLOG_BYINDX\n");
             int index = m_in.plog_int;
             struct plog_pair * pair = &plog_table.content[index];
             mp->mp_reply.plog_ctime = pair->c_time;
             mp->mp_reply.plog_ttime = pair->t_time;
             break;
 
-        case GET_PLOG_SIZE:
-            printf("plog was called with GET_PLOG_SIZE\n");
-            mp->mp_reply.plog_int = PLOG_TABLE_SIZE;
+        case GET_PLOG_BYPID:
+            printf("plog was called with GET_PLOG_BYPID\n");
+            int pid = m_in.plog_int;
+            break;
+
     }    
     return 0;
 }
