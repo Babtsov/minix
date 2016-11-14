7e5923734c1d83aee27a8494a14b0e7adf977246
Babtsov <ben.babtsov@ufl.edu>
2016-11-14 14:42:45 +0000

    make the kernel print its arguments

--- kernel/system/do_plog.c
+++ kernel/system/do_plog.c
@@ -3,17 +3,16 @@
 #include <minix/endpoint.h>
 
 #include <string.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <assert.h>
-#include <libexec.h>
-#include <a.out.h>
-#include <minix/com.h>
-#include <machine/vmparam.h>
-#include <minix/u64.h>
 #include <minix/type.h>
 
 int do_plog(struct proc *caller_ptr, message *m_ptr) {
-    printf("Kernel: hello\n");
+    int pid = m_ptr->m1_i1;
+    int proc_index = m_ptr->m1_i2;
+    bool enabled = m_ptr->m1_i3;
+    printf("kernel: pid: %d index: %d. enabled %d\n",pid, proc_index, enabled);
+    printf("kernel: bye\n");     
     return(OK);
 }
