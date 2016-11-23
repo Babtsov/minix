87121282bde9061caa260d3928635e537002129f
Babtsov <ben.babtsov@ufl.edu>
2016-11-23 03:43:28 +0000

    bring back the table printing

--- kernel/system/do_plog.c
+++ kernel/system/do_plog.c
@@ -14,8 +14,14 @@ int do_plog(struct proc *caller_ptr, message *m_ptr) {
     int pid = m_ptr->m1_i1;
     int proc_index = m_ptr->m1_i2;
     bool enabled = m_ptr->m1_i3;
-    if (pid == INT_MIN && proc_index == INT_MIN) { // serve request to print plog
-        print_plog();
+    if (pid == INT_MIN && proc_index == INT_MIN) { // serve request to print plog_pid
+        printf("PLOG TABLE\n");
+        for (int i = 0; i < PLOG_BUFFER_SIZE; i++) {
+            struct plog_entry * entry = &(plog.buffer[i]);
+            char * from_state = fmt_proc_state(entry->from);
+            char * to_state = fmt_proc_state(entry->to);
+            printf("PID%d\t%ld\t%s\t%s\n",entry->proc_pid, entry->time_stamp, from_state, to_state);
+        }  
         return(OK);
     }
     struct proc * entry = proc_addr(proc_index);
