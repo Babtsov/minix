b8a7523837c3b31705828fe23b39b88db14a51f4
Babtsov <ben.babtsov@ufl.edu>
2016-11-28 04:34:24 +0000

    more stuff

--- lib/libc/sys-minix/state_logger.c
+++ lib/libc/sys-minix/state_logger.c
@@ -1,12 +1,15 @@
 #include <lib.h>
 #include <unistd.h>
+#include <stdio.h>
 
 int plog_state_start(int PID) {
     printf("plog state start\n");
+    message m;
     return _syscall(PM_PROC_NR, STATE_LOGGER, &m);
 }
 
 int plog_state_stop(int PID) {
     printf("plog state stop\n");
+    message m;
     return _syscall(PM_PROC_NR, STATE_LOGGER, &m);
 }
--- lib/libsys/sys_state_logger.c
+++ lib/libsys/sys_state_logger.c
@@ -1,5 +1,6 @@
 #include <syslib.h>
 
 int sys_state_logger(int processId, int processIndex) {
+    message m;
     return _kernel_call(SYS_STATE_LOGGER, &m);
 }
